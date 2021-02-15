2c3a07ca694800f073146a5b670a36409329b72a
Longraf <32570545+Longraf@users.noreply.github.com>
2021-02-15 22:27:07 +0300

    Initial commit

---
+++ src/analytics.js
@@ -0,0 +1,11 @@
+let cl = (string) => {
+  console.log(string)
+}
+
+cl('analytics');
+
+// async function start() {
+//   return await Promise.resolve('async is working')
+// }
+//
+// start().then(console.log)
